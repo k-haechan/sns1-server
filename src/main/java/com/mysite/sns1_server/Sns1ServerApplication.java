79d44bad645e5b051e05a7c6b11e31df5aaa600d
k-haechan <gibbm1127@naver.com>
2025-07-02 15:30:40 +0900

    chore: Spring 프로젝트 생성

---
+++ src/main/java/com/mysite/sns1_server/Sns1ServerApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sns1_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sns1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sns1ServerApplication.class, args);
+	}
+
+}
