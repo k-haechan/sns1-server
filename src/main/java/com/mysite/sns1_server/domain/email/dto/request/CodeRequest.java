9597b3539d9d9068e2341a24956a0b7e75f9fd92
k-haechan <gibbm1127@naver.com>
2025-07-05 01:14:07 +0900

    feat: EmailService를 요청하고 결과를 반환하는 컨트롤러 작성 - 유효성 검증 로직 추가

---
+++ src/main/java/com/mysite/sns1_server/domain/email/dto/request/CodeRequest.java
@@ -0,0 +1,11 @@
+package com.mysite.sns1_server.domain.email.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record CodeRequest(
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Schema(description = "이메일 인증을 위한 이메일 주소", example = "testUser@email.com")
+	String email
+) {
+}
