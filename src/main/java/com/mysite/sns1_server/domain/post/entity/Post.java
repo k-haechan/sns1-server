9d7e2906508537da21e196d2ff87aeba14d14939
k-haechan <gibbm1127@naver.com>
2025-07-22 18:33:29 +0900

    refactor: Post클래스에서 BaseEntity 상속 제거 - builder 패턴을 이용할 때 제약이 많아서 상속 제거했다.

--- src/main/java/com/mysite/sns1_server/domain/post/entity/Post.java
+++ src/main/java/com/mysite/sns1_server/domain/post/entity/Post.java
@@ -1,22 +1,46 @@
 package com.mysite.sns1_server.domain.post.entity;
 
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import com.mysite.sns1_server.domain.member.entity.Member;
 import com.mysite.sns1_server.domain.post.dto.PostRequest;
 import com.mysite.sns1_server.global.aws.s3.service.S3Service;
-import com.mysite.sns1_server.global.baseEntity.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post extends BaseEntity {
+public class Post {
+	@Id
+	@Setter(AccessLevel.PROTECTED)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private Instant createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private Instant updatedAt;
 
 	@Column(nullable = false, length = 500)
 	private String title;
