95c8d6e3c93b935f66491febae9496b1c474770f
김해찬 <gibbm1127@naver.com>
2025-07-29 19:45:56 +0900

    refactor: 필드 이름 변경 - following, follower

--- src/main/java/com/mysite/sns1_server/domain/follow/entity/Follow.java
+++ src/main/java/com/mysite/sns1_server/domain/follow/entity/Follow.java
@@ -10,18 +10,31 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
 public class Follow extends BaseEntity {
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "requester_id", nullable = false)
-	private Member requester; // 팔로우 요청을 보낸 회원
+	@JoinColumn(name = "following_id", nullable = false)
+	private Member following; // 팔로우 요청을 보낸 회원
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "target_id", nullable = false)
-	private Member target; // 팔로우 요청을 받은 회원
+	@JoinColumn(name = "follower_id", nullable = false)
+	private Member follower; // 팔로우 요청을 받은 회원
 
+	@Setter
 	@Enumerated(EnumType.STRING)
 	private FollowStatus status; // 팔로우 상태 (요청, 수락, 거절 등)
+
+	public static Follow create(Member following, Member follower) {
+		Follow follow = new Follow();
+		follow.following = following;
+		follow.follower = follower;
+
+		follow.status = FollowStatus.REQUESTED; // 초기 상태는 요청됨
+		return follow;
+	}
 }
