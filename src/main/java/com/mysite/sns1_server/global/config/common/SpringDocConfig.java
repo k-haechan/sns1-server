b874a5d6d578d196b956b2b69a21dc117943855f
김해찬 <gibbm1127@naver.com>
2025-07-30 15:15:04 +0900

    chore: 배포환경에 맞는 swagger설정 추가

--- src/main/java/com/mysite/sns1_server/global/config/common/SpringDocConfig.java
+++ src/main/java/com/mysite/sns1_server/global/config/common/SpringDocConfig.java
@@ -1,15 +1,45 @@
 package com.mysite.sns1_server.global.config.common;
 
+import java.util.List;
+
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API 서버", version = "v1"))
+@Profile({"dev", "prod"}) // 필요 시 설정
 public class SpringDocConfig {
+
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		io.swagger.v3.oas.models.servers.Server server = new Server();
+
+		if ("prod".equals(activeProfile)) {
+			server.url("https://api.sns1.haechan.site");
+			server.setUrl("https://api.sns1.haechan.site");
+			server.setDescription("배포 서버");
+		} else {
+			server.setUrl("http://localhost:8080");
+			server.setDescription("로컬 개발 서버");
+		}
+
+		return new OpenAPI()
+			.info(new Info()
+				.title("API 서버")
+				.version("v1")
+			)
+			.servers(List.of(server));
+	}
+
 	@Bean
 	public GroupedOpenApi groupApiV1() {
 		return GroupedOpenApi.builder()
